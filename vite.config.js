ab831bd837823b8419313a3206b0a40592780233
Sam <affum3331@gmail.com>
2026-01-04 18:11:32 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Replace 'portfolio' with your repo name
+  base: '/portfolio', // Replace 'portfolio' with your repo name
 })
